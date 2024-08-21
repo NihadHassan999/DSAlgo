b446503f274054f76dcb95d33b255db18e1c5910
NihadHassanTech <nihad.hassan@techversantinfotech.com>
2024-08-21 15:35:58 +0530

    linked list solution added

--- LinkedList/ReverseLinkedList.py
+++ LinkedList/ReverseLinkedList.py
@@ -19,6 +19,26 @@ Constraints:
 -1000 <= Node.val <= 1000
 
 '''
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 
 class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:     
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev, curr = None, head
+
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+        return prev 
+
+'''
+
+T o(n) M o(1)
+not using any data structures just pointers
+
+'''
