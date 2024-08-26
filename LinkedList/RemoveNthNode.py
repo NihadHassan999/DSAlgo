ffe99c1a1585424f3c3ae0192f148e6c7c2046ab
NihadHassanTech <nihad.hassan@techversantinfotech.com>
2024-08-26 16:39:08 +0530

    added copy list and remove nth node updation

--- LinkedList/RemoveNthNode.py
+++ LinkedList/RemoveNthNode.py
@@ -24,7 +24,7 @@ Output: [2]
 '''
 # Definition for singly linked list
 class ListNode:
-    def __init__(self, val=0, next = None)
+    def __init__(self, val=0, next = None):
         self.val = val
         self.next = next
 
