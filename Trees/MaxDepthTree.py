0fb62bfa3583adafade965d06b0d471bab01245f
NihadHassanTech <nihad.hassan@techversantinfotech.com>
2024-09-20 16:57:01 +0530

    added maxDepth solution

---
+++ Trees/MaxDepthTree.py
@@ -0,0 +1,81 @@
+'''
+
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node. 
+
+Example 1:
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+Example 2:
+
+Input: root = [1,null,2]
+Output: 2
+
+'''
+
+####################################
+#
+# SOLUTION 1 : Using Recursion DFS
+#
+####################################
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxDepth(self, root:TreeNode) -> int:
+        if not root:
+            return 0
+        
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(self.right))
+
+'''
+
+Time Complexity : O(n)
+Space Complexity : O(h)
+
+'''
+
+######################################
+#
+# SOLUTION 2 : Using Iterative BFS
+#
+######################################
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+from collections import deque
+
+class Solution:
+    def maxDepth(root: TreeNode) -> int:
+        level = 0
+        q = deque([root])
+
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            
+            level += 1
+
+        return level
+    
+'''
+
+Time Complexity : O(n)
+Space Complexity : O(h)
+
+'''
