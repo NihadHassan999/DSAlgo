2d8424fa3f17d5eaf44f6921e4bb2bdfe470c383
NihadHassan999 <nihad99@gmail.com>
2024-10-12 10:57:47 +0530

    added validPalindrome

---
+++ Two_Pointers/validPalindrome.py
@@ -0,0 +1,60 @@
+'''
+Is Palindrome
+Given a string s, return true if it is a palindrome, otherwise return false.
+
+A palindrome is a string that reads the same forward and backward. It is also case-insensitive and ignores all non-alphanumeric characters.
+
+Example 1:
+
+Input: s = "Was it a car or a cat I saw?"
+
+Output: true
+Explanation: After considering only alphanumerical characters we have "wasitacaroracatisaw", which is a palindrome.
+
+Example 2:
+
+Input: s = "tab a cat"
+
+Output: false
+Explanation: "tabacat" is not a palindrome.
+
+Constraints:
+
+1 <= s.length <= 1000
+s is made up of only printable ASCII characters.
+'''
+
+# 1) using two pointers l and r to traverse to mid of list
+# 2) create helper function for alphanum using ord
+# 3) while alphanum increment and decrement the pointers
+# 4) finally update the pointers the continue the loop
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+
+        while l < r:
+
+            while l < r and not self.alphanum(s[l]):
+                l += 1
+
+            while r > l and not self.alphanum(s[r]):
+                r -= 1
+
+            if s[l].lower() != s[r].lower():
+                return False
+
+            l, r = l + 1, r - 1
+
+        return True  
+
+
+    def alphanum(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or
+                ord('a') <= ord(c) <= ord('z') or
+                ord('0') <= ord(c) <= ord('9'))
+
+'''
+Time Complexity : O(n)
+Space Complexity: O(1)
+'''
