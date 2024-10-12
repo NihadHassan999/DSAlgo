18865befdb51328b96fe8d9e3c23ad79c7bdac21
NihadHassan999 <nihad99@gmail.com>
2024-10-12 11:35:25 +0530

    added twoSumII

---
+++ Two_Pointers/twoSumII.py
@@ -0,0 +1,33 @@
+'''
+Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+Your solution must use only constant extra space.
+'''
+#1) use two pointers to traverse (sorted is advantage)
+#2) find curr sum using curr pointers
+#3) if currsum is less than target, left pointer increment
+#4) if currsum is greater than target, right pointer decrement
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+
+        while l < r:
+            currSum = numbers[l] + numbers[r]
+            if currSum > target:
+                r -= 1
+            elif currSum < target:
+                l += 1
+            else:
+                return [l + 1, r + 1]
+        return []
+
+
+'''
+Time Complexity : O(n)
+Space Complexity : O(1)
+'''
