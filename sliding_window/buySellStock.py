69d83eb510ba603082a00647a7e8e0bc33a611fb
NihadHassan999 <nihad99@gmail.com>
2024-10-15 08:03:53 +0530

    added buysellstock

---
+++ sliding_window/buySellStock.py
@@ -0,0 +1,53 @@
+'''
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+ 
+
+Constraints:
+
+1 <= prices.length <= 105
+0 <= prices[i] <= 104
+'''
+
+'''
+-> create empty maxp and pointer values should point to 0 and 1 index
+-> through length of list, if current buy price<next price, then obtain maxP 
+-> else, next price becomes current buy price
+-> finally finish the loop by incrementing for next iteration
+'''
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxP = 0
+        l, r = 0, 1
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxP = max(maxP, profit)
+            else:
+                l = r
+            r += 1
+        return maxP
+
+'''
+Time Complexity : O(n)
+Space Complexity : O(1)
+'''
