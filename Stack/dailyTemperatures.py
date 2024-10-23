81a4c207ea54a85fc696f1599426e7a64a624ceb
NihadHassan999 <nihad99@gmail.com>
2024-10-23 08:08:24 +0530

    added dailytemperatures

---
+++ Stack/dailyTemperatures.py
@@ -0,0 +1,56 @@
+'''
+739. Daily Temperatures
+Solved
+Medium
+Topics
+Companies
+Hint
+Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+
+ 
+
+Example 1:
+
+Input: temperatures = [73,74,75,71,69,72,76,73]
+Output: [1,1,4,2,1,1,0,0]
+Example 2:
+
+Input: temperatures = [30,40,50,60]
+Output: [1,1,1,0]
+Example 3:
+
+Input: temperatures = [30,60,90]
+Output: [1,1,0]
+ 
+
+Constraints:
+
+1 <= temperatures.length <= 105
+30 <= temperatures[i] <= 100
+'''
+
+'''
+-> create res with 0 default for len(temperatures) and stack
+-> enumerate through temperatures
+-> if stack is not empty and curr temp > highest temp in stack, then pop the current temp and index 
+-> get the difference of index and add to res
+-> if not, append curr temp and index to stack
+-> return res
+'''
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0] * len(temperatures)
+        stack = []
+
+        for i, t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                stackT, stackInd = stack.pop()
+                res[stackInd] = i - stackInd
+            stack.append((t,i))
+        return res
+
+'''
+Time complexity : O(n)
+Space complexity : O(n)
+'''
