221520a06bb8639b5d78553d21a1c83882d39ecf
NihadHassan999 <nihad99@gmail.com>
2024-10-25 16:48:40 +0530

    added searchRotatedSorted

---
+++ Binary_Search/searchRotatedSorted.py
@@ -0,0 +1,63 @@
+'''
+Find Target in Rotated Sorted Array
+Solved 
+You are given an array of length n which was originally sorted in ascending order. It has now been rotated between 1 and n times. For example, the array nums = [1,2,3,4,5,6] might become:
+
+[3,4,5,6,1,2] if it was rotated 4 times.
+[1,2,3,4,5,6] if it was rotated 6 times.
+Given the rotated sorted array nums and an integer target, return the index of target within nums, or -1 if it is not present.
+
+You may assume all elements in the sorted rotated array nums are unique,
+
+A solution that runs in O(n) time is trivial, can you write an algorithm that runs in O(log n) time?
+
+Example 1:
+
+Input: nums = [3,4,5,6,1,2], target = 1
+
+Output: 4
+Example 2:
+
+Input: nums = [3,5,6,0,1,2], target = 4
+
+Output: -1
+Constraints:
+
+1 <= nums.length <= 1000
+-1000 <= nums[i] <= 1000
+-1000 <= target <= 1000
+
+'''
+
+'''
+-> l, r is initialised
+-> since its sorted at a pivot point, we need to check through both left and right sorted halves
+-> find mid, if mid == target, then return mid 
+-> if l pointer value <= mid value, its left sorted and check there
+-> else, its right shorted and check there
+'''
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[l] <= nums[m]:
+                if target < nums[l] or target > nums[m]:
+                    l = m + 1
+                else:
+                    r = m - 1
+            else:
+                if target < nums[m] or target > nums[r]:
+                    r = m - 1
+                else:
+                    l = m + 1
+        return -1
+    
+'''
+Time complexity : O(logn)
+Space complexity : O(1)
+'''
