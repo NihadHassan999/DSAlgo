c2dd286bd8f2a42978ea319862d462ba107904dc
NihadHassan999 <nihad99@gmail.com>
2024-11-08 10:23:11 +0530

    completed wordSearchI

--- Backtracking/wordSearchI.py
+++ Backtracking/wordSearchI.py
@@ -6,8 +6,6 @@ For the word to be present it must be possible to form it with a path in the boa
 
 Example 1:
 
-
-
 Input: 
 board = [
   ["A","B","C","D"],
@@ -18,8 +16,6 @@ word = "CAT"
 
 Output: true
 Example 2:
-
-
 
 Input: 
 board = [
@@ -46,9 +42,45 @@ Otherwise, continue until all paths are exhausted, returning `False` if the word
 -> mark as #, get res = OR cases of all backtracking, reassign from # to original value
 -> Finally loop through and assign dfs()
 '''
-
+from typing import List
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         # define the rows and cols indices
+        ROWS, COLS = len(board), len(board[0])
         
+        # dfs function to recursively search
+        def dfs(r, c, i):        
+          # base case to stop 
+          if i == len(word):
+            return True
+
+          # false scenarios OR cases
+          if (r < 0 or c < 0 or board[r][c] != word[i] or
+              board[r][c] == '#' or r >= ROWS or c >= COLS):
+            return False
+          
+          # mark as #
+          board[r][c] == '#'
+          # Backtracking traversal
+          res = (dfs(r + 1, c, i + 1) or
+                 dfs(r - 1, c, i + 1) or
+                 dfs(r, c + 1, i + 1) or
+                 dfs(r, c - 1, i + 1))
+          # reassign value back
+          board[r][c] = word[i]
+          return res
         
+        # run the dfs
+        for r in range(ROWS):
+          for c in range(COLS):
+            if dfs(r, c, 0):
+              return True
+        
+        return False  
+
+'''
+Time Complexity : O(ROWS x COLS x 4^L)
+Space Complexity : O(L)
+'''        
+          
+           
