032d5cf2cd9593c7d1e2d50235d64d1f2958e9ce
NihadHassan999 <nihad99@gmail.com>
2024-11-11 15:33:06 +0530

    added prefixTree

---
+++ Tries/prefixTree.py
@@ -0,0 +1,83 @@
+'''
+Implement Prefix Tree
+A prefix tree (also known as a trie) is a tree data structure used to efficiently store and retrieve keys in a set of strings. Some applications of this data structure include auto-complete and spell checker systems.
+
+Implement the PrefixTree class:
+
+PrefixTree() Initializes the prefix tree object.
+void insert(String word) Inserts the string word into the prefix tree.
+boolean search(String word) Returns true if the string word is in the prefix tree (i.e., was inserted before), and false otherwise.
+boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+Example 1:
+
+Input: 
+["Trie", "insert", "dog", "search", "dog", "search", "do", "startsWith", "do", "insert", "do", "search", "do"]
+
+Output:
+[null, null, true, false, true, null, true]
+
+Explanation:
+PrefixTree prefixTree = new PrefixTree();
+prefixTree.insert("dog");
+prefixTree.search("dog");    // return true
+prefixTree.search("do");     // return false
+prefixTree.startsWith("do"); // return true
+prefixTree.insert("do");
+prefixTree.search("do");     // return true
+Constraints:
+
+1 <= word.length, prefix.length <= 1000
+word and prefix are made up of lowercase English letters.
+'''
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
+class PrefixTree:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    '''
+    -> check if it exists in children
+    -> if not, create trienode
+    -> if exists, update node
+    -> is_end make true for end of word
+    '''
+    def insert(self, word: str) -> None:
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                node.children[c] = TrieNode()
+            node = node.children[c]
+        node.is_end = True
+
+    '''
+    -> same as insert(), except we return bool
+    '''
+    def search(self, word: str) -> bool:
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        return node.is_end
+
+    '''
+    -> same as search(), except that we use search prefix instead of word
+    '''
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for c in prefix:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        return True
+        
+'''
+Time complexity : O(n)
+Space complexity : O(n x L)
+'''
+        
