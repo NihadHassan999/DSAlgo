719e76b6e11af440cd0b2809d533ae1c20a0e10c
NihadHassan999 <nihad99@gmail.com>
2024-11-15 11:09:24 +0530

    added noOfIslands

---
+++ Graphs/noOfIslands.py
@@ -0,0 +1,64 @@
+'''
+Count Number of Islands
+Given a 2D grid grid where '1' represents land and '0' represents water, count and return the number of islands.
+
+An island is formed by connecting adjacent lands horizontally or vertically and is surrounded by water. You may assume water is surrounding the grid (i.e., all the edges are water).
+
+Example 1:
+
+Input: grid = [
+    ["0","1","1","1","0"],
+    ["0","1","0","1","0"],
+    ["1","1","0","0","0"],
+    ["0","0","0","0","0"]
+  ]
+Output: 1
+Example 2:
+
+Input: grid = [
+    ["1","1","0","0","1"],
+    ["1","1","0","0","1"],
+    ["0","0","1","0","0"],
+    ["0","0","0","1","1"]
+  ]
+Output: 4
+Constraints:
+
+1 <= grid.length, grid[i].length <= 100
+grid[i][j] is '0' or '1'.
+'''
+
+'''
+-> get rows,cols, directions, set initial islands count
+-> iterate through and find the islands and perform dfs on them and increment
+-> create dfs function : base case out of bounds or visited check, assign visited, dfs in all directions
+'''
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        ROWS, COLS = len(grid), len(grid[0])
+        islands = 0
+
+        def dfs(r, c):
+            if (r < 0 or c < 0 or r >= ROWS or 
+                c >= COLS or grid[r][c] == "0"
+            ):
+                return
+                
+            grid[r][c] = "0"
+            for dr, dc in directions:
+                dfs(r + dr, c + dc)
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1":
+                    dfs(r, c)
+                    islands += 1
+
+        return islands
+
+'''
+Time complexity : O(n * m)
+Space complexity : O(n * m)
+'''
