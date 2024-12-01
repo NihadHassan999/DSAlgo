223628fa95a9cba9c22f04256b8f01857e1e2e84
NihadHassan999 <nihad99@gmail.com>
2024-12-01 12:31:54 +0530

    added connectedDSU

---
+++ Graphs/connectedDSU.py
@@ -0,0 +1,77 @@
+'''
+Number of Connected Components in an Undirected Graph
+Solved 
+There is an undirected graph with n nodes. There is also an edges array, where edges[i] = [a, b] means that there is an edge between node a and node b in the graph.
+
+The nodes are numbered from 0 to n - 1.
+
+Return the total number of connected components in that graph.
+
+Example 1:
+
+Input:
+n=3
+edges=[[0,1], [0,2]]
+
+Output:
+1
+Example 2:
+
+Input:
+n=6
+edges=[[0,1], [1,2], [2,3], [4,5]]
+
+Output:
+2
+Constraints:
+
+1 <= n <= 100
+0 <= edges.length <= n * (n - 1) / 2
+'''
+
+class DSU:
+    '''
+    creates parent list and rank list
+    '''
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [1] * n
+
+    '''
+    get grandfather and assign father node
+    '''
+    def find(self, node):
+        cur = node
+        while cur != self.parent[cur]:
+            self.parent[cur] = self.parent[self.parent[cur]]
+            cur = self.parent[cur]
+        return cur
+    
+    '''
+    v rank should be lower than u rank
+    '''
+    def union(self, u, v):
+        pu = self.find(u)
+        pv = self.find(v)
+        if pu == pv:
+            return False
+        if self.rank[pv] > self.rank[pu]:
+            pu, pv = pv, pu
+        self.parent[pv] = pu
+        self.rank[pu] += self.rank[pv]
+        return True
+
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        dsu = DSU(n)
+        res = n
+        for u, v in edges:
+            if dsu.union(u, v):
+                res -= 1
+        return res
+        
+'''
+Time complexity : O(n+E⋅α(n))
+Space complexity : O(n+E)
+'''
